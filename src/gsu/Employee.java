4e87181594fa9649a4f9eaeb65c45667ac159cfe
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-04 14:39:46 +0300

    feat: add constructors and fields

--- src/gsu/Employee.java
+++ src/gsu/Employee.java
@@ -1,4 +1,24 @@
 package gsu;
 
 public class Employee {
+
+    private String name;
+    private int payment;
+    private boolean kids;
+
+    public Employee() {
+        this("",0, false);
+    }
+    public Employee(String name, int payment, boolean kids) {
+        super();
+        this.name = name;
+        this.payment = payment;
+        this.kids = kids;
+    }
+    public Employee(String name, int payment) {
+        this.name = name;
+        this.payment = payment;
+        this.kids = false;
+    }
+
 }
