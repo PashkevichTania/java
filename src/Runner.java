aa18f4205530edb562514841a43535df83822fb5
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 11:53:26 +0300

    feat: add new objects

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,10 @@
+import by.gsu.mslab.*;
+
 public class Runner {
 
     public static void main(String[] args) {
-	// write your code here
+	 Material steel = new Material("steel", 100);
+	 Subject wire = new Subject("wire",10,steel);
+	 System.out.println(wire.toString());
     }
 }
