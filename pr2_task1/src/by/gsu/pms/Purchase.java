ade4e9968d0bb5ea7b5c5bcc0866ee6c2258a876
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 13:37:04 +0300

    feat: add fields and constructors

--- pr2_task1/src/by/gsu/pms/Purchase.java
+++ pr2_task1/src/by/gsu/pms/Purchase.java
@@ -1,4 +1,17 @@
 package by.gsu.pms;
 
 public class Purchase {
+    private String name;
+    private double price;
+    private int number;
+
+    public Purchase(String name, double price, int number){
+        super();
+        this.name = name;
+        this.number = number;
+        this.price = price;
+    }
+    public Purchase(){
+        this("",0,0);
+    }
 }
