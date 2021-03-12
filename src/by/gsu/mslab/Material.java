2790319c65047df9709dd395b0edf3d28fed8999
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 11:16:16 +0300

    refactor: add fields, getters and setters

--- src/by/gsu/mslab/Material.java
+++ src/by/gsu/mslab/Material.java
@@ -1,4 +1,37 @@
 package by.gsu.mslab;
 
 public class Material {
+    //fields
+
+    private String name;
+    private int density;
+
+    //constructors
+
+    public Material() {
+        this("",0);
+    }
+    public Material(String name, int density) {
+        super();
+        this.name = name;
+        this.density = density;
+    }
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDensity() {
+        return density;
+    }
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
 }
