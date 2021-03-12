f4f58ab1860ff91bc3c082e94b1d8459fbba6ab0
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 12:03:44 +0300

    feat: task complete

--- src/Runner.java
+++ src/Runner.java
@@ -3,8 +3,14 @@ import by.gsu.mslab.*;
 public class Runner {
 
     public static void main(String[] args) {
-	 Material steel = new Material("steel", 100);
-	 Subject wire = new Subject("wire",10,steel);
+	 Material steel = new Material("steel", 7850);
+	 Material copper = new Material("copper", 8500);
+	 Subject wire = new Subject("wire",0.3,steel);
+
+	 System.out.println(steel.toString());
+	 System.out.println(copper.toString());
+	 System.out.println(wire.toString());
+	 wire.setMaterial(copper);
 	 System.out.println(wire.toString());
     }
 }
