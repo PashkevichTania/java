4d38424fd1fbb9f3793095901c3822e45294b3bc
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 11:59:31 +0300

    fix: change fields to double

--- src/by/gsu/mslab/Material.java
+++ src/by/gsu/mslab/Material.java
@@ -4,14 +4,14 @@ public class Material {
     //fields
 
     private String name;
-    private float density;
+    private double density;
 
     //constructors
 
     public Material() {
         this("",0);
     }
-    public Material(String name, float density) {
+    public Material(String name, double density) {
         super();
         this.name = name;
         this.density = density;
@@ -27,11 +27,11 @@ public class Material {
         this.name = name;
     }
 
-    public float getDensity() {
+    public double getDensity() {
         return density;
     }
 
-    public void setDensity(float density) {
+    public void setDensity(double density) {
         this.density = density;
     }
 
--- src/by/gsu/mslab/Subject.java
+++ src/by/gsu/mslab/Subject.java
@@ -3,12 +3,12 @@ package by.gsu.mslab;
 public class Subject {
 
     private String name;
-    private float volume;
+    private double volume;
 
     //HAS A relation
     private Material material;
 
-    public Subject(String name, float volume, Material material){
+    public Subject(String name, double volume, Material material){
         this.material = material;
         this.name = name;
         this.volume =volume;
@@ -24,11 +24,11 @@ public class Subject {
         this.name = name;
     }
 
-    public float getVolume() {
+    public double getVolume() {
         return volume;
     }
 
-    public void setVolume(float volume) {
+    public void setVolume(double volume) {
         this.volume = volume;
     }
 
@@ -40,7 +40,7 @@ public class Subject {
         this.material = material;
     }
 
-    public float getMass(){
+    public double getMass(){
         return material.getDensity()*this.volume;
     }
     @Override
