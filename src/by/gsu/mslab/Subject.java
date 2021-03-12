059086e8c7d0c1af532e9c7e0bc4b19f5c40bb54
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-12 11:29:46 +0300

    refactor: add Subject fields and constructor

--- src/by/gsu/mslab/Subject.java
+++ src/by/gsu/mslab/Subject.java
@@ -1,4 +1,15 @@
 package by.gsu.mslab;
 
 public class Subject {
+
+    private String name;
+    private int volume;
+
+    //HAS A relation
+    private Material material;
+
+    public Subject(){
+        this.material=new Material();
+    }
+
 }
