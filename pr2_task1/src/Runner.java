678c874338f7b8181afbfc8dbe8e9d481a22c542
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-25 18:44:15 +0300

    refactor

--- pr2_task1/src/Runner.java
+++ pr2_task1/src/Runner.java
@@ -27,27 +27,5 @@ public class Runner {
     System.out.println(purchases[0].equals(purchases[2]));
     System.out.println(pringles);
     System.out.println(nutella);
-        String token1 = "";
 
-        // create Scanner inFile1
-        Scanner inFile1 = new Scanner(new File("in.txt")).useDelimiter(",\\s*");
-
-        // Original answer used LinkedList, but probably preferable to use ArrayList in most cases
-        // List<String> temps = new LinkedList<String>();
-        List<String> temps = new ArrayList<String>();
-
-        // while loop
-        while (inFile1.hasNext()) {
-            // find next line
-            token1 = inFile1.next();
-            temps.add(token1);
-        }
-        inFile1.close();
-
-        String[] tempsArray = temps.toArray(new String[0]);
-
-        for (String s : tempsArray) {
-            System.out.println(s);
-        }
-    }
 }
