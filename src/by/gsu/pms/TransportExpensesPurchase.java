9a08fcfd007477af6350da726297fea4f76b0111
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-26 12:51:28 +0300

    add TransportExpensesPurchase

---
+++ src/by/gsu/pms/TransportExpensesPurchase.java
@@ -0,0 +1,33 @@
+package by.gsu.pms;
+
+public class TransportExpensesPurchase extends AbstractPurchase{
+    private int transportExpenses;
+
+
+    public TransportExpensesPurchase(Commodity commodity, int number, int transportExpenses) {
+        super(commodity, number);
+        this.transportExpenses = transportExpenses;
+    }
+
+    public TransportExpensesPurchase(int transportExpenses) {
+        this.transportExpenses = transportExpenses;
+    }
+
+    public int getTransportExpenses() {
+        return transportExpenses;
+    }
+
+    public void setTransportExpenses(int transportExpenses) {
+        this.transportExpenses = transportExpenses;
+    }
+
+    @Override
+    public int getCost() {
+        return super.getCost()+transportExpenses;
+    }
+
+    @Override
+    protected String fieldsToString() {
+        return super.fieldsToString() + transportExpenses;
+    }
+}
