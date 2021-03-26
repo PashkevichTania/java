50c0bdbddad6e690c8e58d48b3f38067ae1000ef
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-26 12:34:44 +0300

    add AbstractPurchase

---
+++ src/by/gsu/pms/AbstractPurchase.java
@@ -0,0 +1,46 @@
+package by.gsu.pms;
+
+public abstract class AbstractPurchase implements Comparable<AbstractPurchase>{
+    private Commodity commodity;
+    private int number;
+
+    public AbstractPurchase(Commodity commodity, int number) {
+        this.commodity = commodity;
+        this.number = number;
+    }
+
+    public AbstractPurchase() {
+
+    }
+
+    public Commodity getCommodity() {
+        return commodity;
+    }
+
+    public void setCommodity(Commodity commodity) {
+        this.commodity = commodity;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCost(){
+        return commodity.getPrice()*number;
+    }
+
+    @Override
+    public final String toString() {
+        return commodity + ";" + number + ";" + getCost();
+    }
+
+
+    @Override
+    public int compareTo(AbstractPurchase purchase) {
+        return purchase.getCost()-this.getCost();
+    }
+}
