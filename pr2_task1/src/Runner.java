fbff9a20b7f4760703824bb0ba36dc2fe00c29bd
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-26 10:15:36 +0300

    finish task1

--- pr2_task1/src/Runner.java
+++ pr2_task1/src/Runner.java
@@ -5,31 +5,38 @@ import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args){
-
-        Purchase[] purchases = {
-
-            new Purchase("milk",30,1),
-            new Purchase("milk",30,3),
-            new Purchase("milk",40,1),
-            new Purchase("almond milk",230,1),
-
-    };
-        DiscountPurchase pringles = new DiscountPurchase("pringles",250,1,10);
-        NumberDiscountPurchase nutella = new NumberDiscountPurchase("nutella",500,20,5.825);
-
-    for (Purchase purchase : purchases) {
-        if (purchase != null){
-            System.out.println(purchase);
-        }
-    }
-    System.out.println(purchases[0].equals(purchases[1]));
-    System.out.println(purchases[0].equals(purchases[2]));
-    System.out.println(pringles);
-    System.out.println(nutella);
+        Purchase maxCostPurchase = new Purchase();
+        boolean isAllEqual= true;
 
         try(Scanner scanner = new Scanner(new FileReader("E:\\programming\\java\\java\\pr2_task1\\src\\in.txt"))){
             scanner.useLocale(Locale.ENGLISH);
-            System.out.println(MyScanner.getClassFromFile(scanner));
+            Purchase[] purchases = {
+
+                    new Purchase("milk",30,1),
+                    new Purchase("milk",30,3),
+                    new Purchase("milk",40,1),
+                    new Purchase("almond milk",230,1),
+                    new DiscountPurchase("pringles",250,1,10),
+                    new NumberDiscountPurchase("nutella",500,20,5.825),
+                    MyScanner.getClassFromFile(scanner),
+            };
+            for (Purchase purchase : purchases) {
+                if (purchase != null){
+                    System.out.println(purchase);
+                    if (maxCostPurchase.getCost() < purchase.getCost()) {
+                        maxCostPurchase = purchase;
+                    }
+                    if (!purchase.equals(purchases[0])) {
+                        isAllEqual = false;
+                    }
+                }
+            }
+            if (isAllEqual) {
+                System.out.println("All equal");
+            } else {
+                System.out.println("All not equal");
+            }
+            System.out.println("Max cost = "+ maxCostPurchase);
         }
         catch (FileNotFoundException e){
             System.err.println("File not found");
