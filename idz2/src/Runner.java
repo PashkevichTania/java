5084fac86b1b8130c633f86246b99b05657ccd8f
PashkevichTania <pashkevichtania2004@gmail.com>
2021-04-12 10:02:51 +0300

    init: start task

---
+++ idz2/src/Runner.java
@@ -0,0 +1,38 @@
+import by.gsu.pms.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        Employee[] employees = {
+
+                new Employee("Sakovich", 4500, true ),
+                null,
+                new Employee("Song", 5000, true),
+                new Employee("Hudson", 3000, true),
+                new Employee("Jhonson", 15000, false),
+                new Employee("Kovalchek", 4000, false),
+                new Employee("Milka", 7000, false),
+                new Employee("Ovalov", 6000, true),
+                new Employee("Volchek", 1000, false),
+                new Employee(),
+        };
+
+        int totalPayment = 0;
+        int totalKids = 0;
+
+        for (Employee employee : employees){
+            if (employee != null){
+                totalPayment+=employee.getPayment();
+                if (employee.haveKids()){
+                    totalKids+=1;
+                }
+            }
+        }
+        int averagePayment = totalPayment/ employees.length;
+        System.out.println("Average payment = " + averagePayment);
+        System.out.println("Employees with kids = " + totalKids);
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
---
+++ idz2/src/by/gsu/pms/Employee.java
@@ -0,0 +1,52 @@
+package by.gsu.pms;
+
+public class Employee {
+
+    private String name;
+    private int payment;
+    private boolean kids;
+
+    public Employee() {
+        this("",0, false);
+    }
+    public Employee(String name, int payment, boolean kids) {
+        super();
+        this.name = name;
+        this.payment = payment;
+        this.kids = kids;
+    }
+    public Employee(String name, int payment) {
+        this.name = name;
+        this.payment = payment;
+        this.kids = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    public void setPayment(int payment) {
+        this.payment = payment;
+    }
+
+    public boolean haveKids() {
+        return kids;
+    }
+
+    public void setKids(boolean kids) {
+        this.kids = kids;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + payment + ";" + kids + ";";
+    }
+}
