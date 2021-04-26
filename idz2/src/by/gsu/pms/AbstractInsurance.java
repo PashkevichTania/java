fd733ef339db99f8ef3402ff7890e749b4800af3
PashkevichTania <pashkevichtania2004@gmail.com>
2021-04-26 19:46:50 +0300

    feat: add AbstractInsurance

--- idz2/src/by/gsu/pms/AbstractInsurance.java
+++ idz2/src/by/gsu/pms/AbstractInsurance.java
@@ -1,5 +1,55 @@
 package by.gsu.pms;
 
-public class AbstractInsurance {
+import java.time.Year;
+
+public class AbstractInsurance implements Comparable<AbstractInsurance> {
+
+    private Year year;
+    private int cost;
+    private double risk;
+
+    protected AbstractInsurance() {
+
+    }
+
+    protected AbstractInsurance(Year year, int cost, double risk) {
+        this.year = year;
+        this.cost = cost;
+        this.risk = risk;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Double getRisk() {
+        return risk;
+    }
+
+    public void setRisk(Double risk) {
+        this.risk = risk;
+    }
+
+    @Override
+    public int compareTo(AbstractInsurance insurance) {
+        return getRisk().compareTo(insurance.getRisk());
+    }
+
+    @Override
+    public String toString() {
+        return this.year + ";" + this.cost + ";" + this.risk;
+    }
 
 }
