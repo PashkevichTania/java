f15e4fe5d52c4545a5dfaa80809fe68f003eb4cb
PashkevichTania <pashkevichtania2004@gmail.com>
2021-04-26 19:57:47 +0300

    feat: start FlatInsurance

--- idz2/src/by/gsu/pms/FlatInsurance.java
+++ idz2/src/by/gsu/pms/FlatInsurance.java
@@ -1,4 +1,18 @@
 package by.gsu.pms;
 
-public class FlatInsurance {
+import java.time.Year;
+
+public class FlatInsurance extends AbstractInsurance {
+
+    private String name;
+
+    public FlatInsurance(String name) {
+        this.name = name;
+    }
+
+    public FlatInsurance(Year year, int cost, double risk, String name) {
+        super(year, cost, risk);
+        this.name = name;
+    }
+
 }
