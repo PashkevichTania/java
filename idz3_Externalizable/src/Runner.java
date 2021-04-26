1a619f84492104f9307f764a239edcfb89462116
PashkevichTania <pashkevichtania2004@gmail.com>
2021-04-26 20:01:59 +0300

    init: start task

---
+++ idz3_Externalizable/src/Runner.java
@@ -0,0 +1,13 @@
+import by.gsu.pms.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+    }
+}
