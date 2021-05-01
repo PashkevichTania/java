8319b43aa65ca2e01a8a909047089d8c47f2def8
PashkevichTania <pashkevichtania2004@gmail.com>
2021-05-01 13:26:00 +0300

    finish task

--- idz2/src/Runner.java
+++ idz2/src/Runner.java
@@ -1,9 +1,37 @@
-import by.gsu.pms.*;
+import by.gsu.pms.insurance.*;
+import by.gsu.pms.insurance.AbstractInsurance;
+import by.gsu.pms.client.Client;
 
+import java.time.Year;
+import java.util.Arrays;
 
 
 public class Runner {
     public static void main(String[] args) {
-
+        AbstractInsurance[] insurances = {
+                new CarInsurance("Touota", "1111", Year.of(2021), 5000, 0.6),
+                new CarInsurance("Smart", "4232", Year.of(2021), 15000, 0.3),
+                new FlatInsurance("Ogorenko7",400, "8775", Year.of(2020), 30000, 0.5),
+                new FlatInsurance("Sviridova8",600, "4338", Year.of(2020), 10000, 0.2),
+        };
+        Client[] clients = {
+                new Client("Ivanov Ivan Ivanovich", Year.of(1960)),
+                new Client("Petrov Ivan Ivanovich", Year.of(1970)),
+                new Client("Slivcev Ivan Ivanovich", Year.of(1980)),
+                new Client("Yakovlev Ivan Ivanovich", Year.of(1990)),
+        };
+        Derivative[] derivatives = {
+                new Derivative(clients[0], insurances[0]),
+                new Derivative(clients[1], insurances[1]),
+                new Derivative(clients[2], insurances[2]),
+                new Derivative(clients[3], insurances[3]),
+        };
+        Arrays.sort(insurances);
+        for (AbstractInsurance insurance: insurances) {
+            System.out.println(insurance);
+        }
+        for (Derivative derivative: derivatives) {
+            System.out.println(derivative);
+        }
         }
     }
