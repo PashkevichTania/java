154d52c138bc504b2644a1103a85268f6349d6df
PashkevichTania <pashkevichtania2004@gmail.com>
2021-10-10 19:41:36 +0300

    feat: add console message

---
+++ idz_7/src/by/gsu/pms/ConsoleMessages.java
@@ -0,0 +1,7 @@
+package by.gsu.pms;
+
+public class ConsoleMessages {
+    public static final String ENTER_THREAD_NUM = "Enter thread num:";
+    public static final String ENTER_A = "Enter a:";
+    public static final String ENTER_OPERATION = "Enter operation (sum, sub, mul):";
+}
