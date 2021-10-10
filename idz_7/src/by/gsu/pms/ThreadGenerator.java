cc03622287453c331d26db12f3b36f042d9792ee
PashkevichTania <pashkevichtania2004@gmail.com>
2021-10-10 19:41:14 +0300

    feat: finish tread generator

--- idz_7/src/by/gsu/pms/ThreadGenerator.java
+++ idz_7/src/by/gsu/pms/ThreadGenerator.java
@@ -14,5 +14,39 @@ public class ThreadGenerator {
         this.a = a;
     }
 
+    public void execute() {
+        long subtaskRange = a / threadNum;
 
+        for (int i = 0; i < threadNum; i++) {
+            long subtaskStart = i * subtaskRange + 1;
+            long subtaskEnd = i * subtaskRange + subtaskRange;
+
+            if ( i == threadNum - 1){
+                subtaskEnd = a;
+            }
+
+            calculators[i] = new Calculator(subtaskStart, subtaskEnd);
+
+            Thread thread = new Thread(calculators[i]);
+            thread.start();
+
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            resultReady = true;
+        }
+    }
+
+    public long getResult() {
+        if (!resultReady) {
+            throw new IllegalStateException("execute() must be executed before getResult() excecution");
+        }
+        long result = operator.getInitialValue();
+        for (Calculator calculator : calculators) {
+            result = operator.doOperation(result, calculator.getResult());
+        }
+        return result;
+    }
 }
